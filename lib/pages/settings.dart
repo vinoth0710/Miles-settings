849c64c95fe9c1f550406159a9504d5958539fdc
DinishibaS <dinishiba@gmail.com>
2021-01-18 23:31:12 +0530

    corrected settings

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:settings_ui/pages/edit_profile.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
